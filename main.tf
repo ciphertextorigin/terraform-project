7b82448b7d5697e92ccef2e92845f38e8c42c810
uzotony1 <82820630+uzotony1@users.noreply.github.com>
2021-12-25 00:36:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 module "lambda-apigw-module" {
-  source  = "jeremygovi/lambda-apigw-module/aws"
-  version = "0.0.3"
-  project_name          = "helloworld-app"
-  source_path           = "./main.js"
-  lambda_function_name  = "my_lambda_function_name"
+  source               = "jeremygovi/lambda-apigw-module/aws"
+  version              = "0.0.3"
+  project_name         = "helloworld-app"
+  source_path          = "./main.js"
+  lambda_function_name = "my_lambda_function_name"
   #lambda_runtime        = "python3.8"
-  lambda_runtime        = "nodejs12.x"
-  api_gateway_name      = "helloworld-app-api-gw"
+  lambda_runtime   = "nodejs12.x"
+  api_gateway_name = "helloworld-app-api-gw"
   environment_variables = {
-    customEnvVariable = "prod" 
+    customEnvVariable = "prod"
   }
 }
+
