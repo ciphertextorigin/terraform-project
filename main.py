e0ca729a664a53c3f2d31be7bc226915ee9dcc02
uzotony1 <82820630+uzotony1@users.noreply.github.com>
2021-12-25 15:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # index.py
 
+
+
 # Declaring the Python Function lambda_handler with event paramater
 def lambda_handler(event, context):
     message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
