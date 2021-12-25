90d3a107527e7a26458c4d15fee68203764ae137
uzotony1 <82820630+uzotony1@users.noreply.github.com>
2021-12-25 16:07:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "lambda-apigw-module" {
   project_name         = "helloworld-app"
   source_path          = "./main.js"
   lambda_function_name = "my_lambda_function_name"
-  #lambda_runtime        = "python3.8"
+  #lambda_runtime      = "python3.8"
   lambda_runtime   = "nodejs12.x"
   api_gateway_name = "helloworld-app-api-gw"
   environment_variables = {
